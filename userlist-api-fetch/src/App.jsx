e8a789e193bb7ad10e199ebdfe8ca9df49caebca
Ashita Thakur Personal <ashitathakur2807@gmail.com>
2026-06-10 22:08:26 +0530

    added async await

--- userlist-api-fetch/src/App.jsx
+++ userlist-api-fetch/src/App.jsx
@@ -1,29 +1,44 @@
 import { useEffect, useState } from 'react'
-
+import "./App.css"
 
 function App() {
 
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => { setUsers(data) });
+    getUser();
+    console.log("Fetching users...");
   }, []);
-  return (
 
-    <div>
-      <h1>User List</h1>
-      {
-        users.map((user) => (
-          <div key={user.id}>
-            <h3>{user.name}</h3>
-            <p>{user.email}</p>
-          </div>
-        ))
-      }
-    </div>
-  )
+
+  async function getUser(){
+      const response= await fetch("https://jsonplaceholder.typicode.com/users");
+      const data=await response.json();
+      setUsers(data);
+      setLoading(false);
+      
+  }
+  if (loading) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div >
+        <h1>User List</h1>
+        {
+          users.map((user) => (
+            <div key={user.id} className='users'>
+              <h3>{user.name}</h3>
+              <p>{user.email}</p>
+              <p>{user.phone}</p>
+            </div>
+          ))
+        }
+      </div>
+
+    )
+
+  }
 }
 
 export default App
